92a0273da5a0e01a7becf39c8f12b11c30419eee
Theodor Kapler <theodor.kapler@student.kit.edu>
2023-06-18 16:09:57 +0200

    Started Loading Data

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-# Change
+import os
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+import segmentation_models_pytorch as smp
+
+# Loading Data
+DATA_DIR = './data/CamVid/'
+
+# load repo with data if it is not exists
+if not os.path.exists(DATA_DIR):
+    print('Loading data...')
+    os.system('git clone https://github.com/alexgkendall/SegNet-Tutorial ./data')
+    print('Done!')
+
