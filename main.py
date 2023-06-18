73f48545ac265d2b96bea3a5b5f7125df4540eab
Theodor Kapler <theodor.kapler@student.kit.edu>
2023-06-18 19:56:34 +0200

    Chenged main

--- main.py
+++ main.py
@@ -4,12 +4,65 @@ import cv2
 import matplotlib.pyplot as plt
 import segmentation_models_pytorch as smp
 
-# Loading Data
+################################################################################
+# Daten laden
+################################################################################
+#-------------------------------------------------------------------------------
+# Daten ins Programm laden
+#-------------------------------------------------------------------------------
 DATA_DIR = './data/CamVid/'
 
-# load repo with data if it is not exists
 if not os.path.exists(DATA_DIR):
     print('Loading data...')
     os.system('git clone https://github.com/alexgkendall/SegNet-Tutorial ./data')
     print('Done!')
+
+# Ordnerpfade der jeweiligen Subsets abgreifen
+x_train_dir = os.path.join(DATA_DIR, 'train')
+y_train_dir = os.path.join(DATA_DIR, 'trainannot')
+
+x_valid_dir = os.path.join(DATA_DIR, 'val')
+y_valid_dir = os.path.join(DATA_DIR, 'valannot')
+
+x_test_dir = os.path.join(DATA_DIR, 'test')
+y_test_dir = os.path.join(DATA_DIR, 'testannot')
+
+# Hilfsfunktion, die mehrere Bilder in einem Subplot darstellt
+# visualize(image_1=img1, image2=img2) kann dann über key und value iterieren
+def visualize(**images):
+    """PLot images in one row."""
+    n = len(images)
+    plt.figure(figsize=(16, 5))
+    for i, (name, image) in enumerate(images.items()):
+        plt.subplot(1, n, i + 1)
+        plt.xticks([])
+        plt.yticks([])
+        plt.title(' '.join(name.split('_')).title())
+        plt.imshow(image)
+    plt.show()
+#-------------------------------------------------------------------------------
+# Dataloader
+#-------------------------------------------------------------------------------
+
+#-------------------------------------------------------------------------------
+# Augmentations
+#-------------------------------------------------------------------------------
+
+
+################################################################################
+# Modell erstellen
+################################################################################
+
+################################################################################
+# Trainieren
+################################################################################
+
+################################################################################
+# Testen
+################################################################################
+
+################################################################################
+# Predictions visualisieren
+################################################################################
+
 
